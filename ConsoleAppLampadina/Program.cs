bdf527cb614f7d17ce3ed7e78b2f5cd351bdbd27
lucrezia sabba <lucrezia.sabba@icloud.com>
2020-10-21 12:57:50 +0200

    Aggiungere i file di progetto.

---
+++ ConsoleAppLampadina/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Luce;
+
+namespace ConsoleAppLampadina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lampadina 1 = new Lampadina("spenta", 5);
+            Console.WriteLine("Quanto è il numero massimo di click che si possono fare?");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(1.Click());
+            }
+        }
+    }
+}
---
+++ Lampadina/Lampadina.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luce
+{
+    public class Lampadina
+    {
+        private int massimoClick;
+        private String stato;
+        private int numeroclick;
+        public Lampadina(int m, String s, int n)
+        {
+            massimoClick = m;
+            stato = s;
+            numeroclick=n;
+        }
+        public string Descrizione()
+        {
+            return "Lo stato della lampadina è " + stato;
+        }
+        public string Click(int massimoClick)
+        {
+            numeroclick++;
+                if(numeroclick>massimoClick)
+                {
+                    stato = "rotta";
+                }
+                else
+                {
+                    if (stato == "accesa")
+                    {
+                        stato = "spenta";
+                    }
+                    else if (stato == "spenta")
+                    {
+                        stato = "accesa";
+                    }
+                }
+            return stato;
+        }
+    }
+}
